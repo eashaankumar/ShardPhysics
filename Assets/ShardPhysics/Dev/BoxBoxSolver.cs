f1559b45650700daa67d924c04f59efc1f5db773
Eashaan Kumar Main PC <6231685+eashaankumar@users.noreply.github.com>
2026-02-17 13:12:20 -0500

    Working on clean box box sat solver

--- Assets/ShardPhysics/Dev/BoxBoxSolver.cs
+++ Assets/ShardPhysics/Dev/BoxBoxSolver.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -10,8 +11,63 @@ public struct BoxBoxSolver
         public float3 tangent;
     }
 
-    public static void Solve(float3 center1, float3 size1, quaternion rot1, float3 center2, float3 size2, quaternion rot2, ContactPoint[] cps)
+    public struct BoxBoxContactPoints
     {
+        public ContactPoint p1;
+        public ContactPoint p2;
+        public ContactPoint p3;
+        public ContactPoint p4;
+    }
 
+    public struct Box
+    {
+        public float3 center;
+        public float3 size;
+        public quaternion rot;
+    }
+
+    public struct BoxAxis
+    {
+        public float3 right;
+        public float3 up;
+        public float3 forward;
+
+        public float3 this[int index]
+        {
+            get
+            {
+                if (index == 0) return right;
+                if (index == 1) return up;
+                if (index == 2) return forward;
+                return float3.zero;
+            }
+            set
+            {
+                if (index == 0) right = value;
+                if (index == 1) up = value;
+                if (index == 2) forward = value;
+
+            }
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Solve(Box box1, Box box2, out BoxBoxContactPoints bbcps)
+    {
+        bbcps = default;
+
+        BoxAxis box1Axis = GetBoxAxis(box1);
+        BoxAxis box2Axis = GetBoxAxis(box2);
+
+        return false;
+    }
+
+    public static BoxAxis GetBoxAxis(Box box)
+    {
+        BoxAxis boxAxis = default;
+        boxAxis.right = math.rotate(box.rot, math.right());
+        boxAxis.up = math.rotate(box.rot, math.up());
+        boxAxis.forward = math.rotate(box.rot, math.forward());
+        return boxAxis;
     }
 }
