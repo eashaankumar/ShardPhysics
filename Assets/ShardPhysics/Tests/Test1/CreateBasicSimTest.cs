da6fdea1eeab12b356193699a0004a3238ea82ba
Eashaan Kumar Main PC <6231685+eashaankumar@users.noreply.github.com>
2026-02-18 00:10:25 -0500

    Basic cube physics sim executes perfectly

--- Assets/ShardPhysics/Tests/Test1/CreateBasicSimTest.cs
+++ Assets/ShardPhysics/Tests/Test1/CreateBasicSimTest.cs
@@ -1,4 +1,8 @@
 using Shard.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Shard.Tests.Test1
@@ -13,13 +17,42 @@ namespace Shard.Tests.Test1
         private void Awake()
         {
             world = new ShardPhysicsWorld();
+            world.gravity = math.down() * 9.81f;
 
-            //box1 = world.CreateBody(new Runtime.Pose { position= box1Trans.position, rotation=box1Trans.rotation}, 1, )
+            NativeList<ShardCollider> colliders = new NativeList<ShardCollider>(Allocator.Temp);
+            colliders.Add(new ShardCollider
+            {
+                localPose=new Runtime.Pose { position=float3.zero, rotation=quaternion.identity },
+                type = ShardColliderType.Box,
+                density = 1.0f,
+                halfExtents = box1Trans.localScale/2,
+            });
+
+            box1 = world.CreateBody(new Runtime.Pose { position = box1Trans.position, rotation = box1Trans.rotation }, 1, colliders.AsArray());
+
+            StartCoroutine(Tick());
         }
 
         private void OnDestroy()
         {
             world.Dispose();
+        }
+
+        IEnumerator Tick()
+        {
+            float dt = 1.0f / 60f;
+            while(true)
+            {
+                yield return new WaitForSeconds(dt);
+
+                world.Simulate(dt);
+
+                if(world.TryGetPose(box1, out var pose))
+                {
+                    box1Trans.position = pose.position;
+                    box1Trans.rotation = pose.rotation;
+                }
+            }
         }
     }
 }
